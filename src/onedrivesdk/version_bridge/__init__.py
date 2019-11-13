50e8294793b4c3913bf24d230703442d7f5773a6
earonesty <erik@q32.com>
2019-11-13 14:51:42 -0500

    Update __init__.py

--- src/onedrivesdk/version_bridge/__init__.py
+++ src/onedrivesdk/version_bridge/__init__.py
@@ -22,7 +22,4 @@
 ------------------------------------------------------------------------------
 '''
 
-import os
-import glob
-modules = glob.glob(os.path.dirname(__file__)+"/*.py")
-__all__ = [ os.path.basename(f)[:-3] for f in modules if not os.path.basename(f).startswith('_')]
+__all__ = ["async_operation_monitor_async", "auth_provider_async", "fragment_upload"]
